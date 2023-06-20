5bb2acd295e448fb9f79a36d74586810b2fe5dae
Ashish Baravaliya <vier.institute35@gmail.com>
2023-06-20 13:46:59 -0400

    feat: credits added to footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-100 py-16">
+    <footer className="bg-primary-100 pt-16 pb-2">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} />
@@ -24,6 +24,50 @@ const Footer = () => {
           <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
           <p>(333)425-6825</p>
         </div>
+      </div>
+      <div>
+      <div
+      className="flex justify-center py-3 mt-8 text-gray-800 border-t border-stone-300">
+        <div>
+          Made by
+          <a
+            href="https://github.com/AshishBarvaliya"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-1 text-gray-900 hover:text-red-500 underline"
+          >
+            Ashish
+          </a>
+          with
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://nextjs.org/"
+            className="px-1 hover:text-red-500 underline"
+          >
+            Nextjs
+          </a>
+          &
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="pl-1 hover:text-red-500 underline"
+          >
+            TailwindCSS
+          </a>
+          . Deployed on
+          <a
+            href="https://www.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="pl-1 hover:text-red-500 underline"
+          >
+            Netlify
+          </a>
+          .
+        </div>
+      </div>
       </div>
     </footer>
   );
